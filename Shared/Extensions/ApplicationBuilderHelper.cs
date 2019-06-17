775481a32c9db37a5d9fcb9404f4ffe582d2f1bb
Rencil Justin Evangelista <renciljustinevangelista@outlook.com>
2019-06-17 20:35:15 +0800

    Added Global Exception Handler

---
+++ Shared/Extensions/ApplicationBuilderHelper.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Dately.Shared.Extensions
+{
+    public static class ApplicationBuilderHelper
+    {
+        public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder app)
+        {
+            return app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = "application/json";
+
+                        var features = context.Features.Get<IExceptionHandlerFeature>();
+
+                        if (context != null)
+                        {
+                            context.Response.Headers.Add("Application-Error", features.Error.Message);
+                            context.Response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+                            context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+                            await context.Response.WriteAsync(JsonConvert.SerializeObject(features.Error.Message));
+                        }
+                    });
+                });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using AutoMapper;
 using Dately.Core;
 using Dately.Core.Models;
 using Dately.Core.Seeds;
 using Dately.Persistence;
+using Dately.Shared.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Dately
@@ -65,7 +59,8 @@ namespace Dately
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
+                app.UseGlobalExceptionHandler();
             }
 
             seed.BeginSeeding();
