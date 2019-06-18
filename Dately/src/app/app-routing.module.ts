322914aa278e7b0f1b9aebe7d0c6d6c8848c444e
Rencil Justin Evangelista <renciljustinevangelista@outlook.com>
2019-06-18 21:40:48 +0800

    Configure Routing

--- Dately/src/app/app-routing.module.ts
+++ Dately/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
+import { UserDetailComponent } from './components/users/user-detail/user-detail.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'index', component: HomeComponent },
+  {
+    path: '',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'users/:id', component: UserDetailComponent },
+      { path: 'users', component: UserListComponent },
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Dately/src/app/app.module.ts
+++ Dately/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserDetailComponent } from './components/users/user-detail/user-detail.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavigationComponent,
+    RegisterComponent,
+    UserDetailComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
